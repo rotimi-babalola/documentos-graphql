3717efa64415a002349e11d9b2e5baf082c0fe88
rotimi-babalola <rotimi.babalola@andela.com>
2019-06-26 20:27:35 +0100

    run model migrations

---
+++ config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const envs = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.TEST_USER,
+    password: process.env.TEST_PASSWORD,
+    database: process.env.TEST_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.PD_USER,
+    password: process.env.PD_PASSWORD,
+    database: process.env.PD_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
+
+module.exports = envs;
